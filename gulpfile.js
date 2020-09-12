0a1a007ac2678a19c0219ef6e16fffb0f7e031c8
AlexDyatlov <aleks78.gooogle@gmail.com>
2020-09-12 23:47:44 +0300

    removed slick added swiper

--- gulpfile.js
+++ gulpfile.js
@@ -93,7 +93,7 @@ gulp.task("style", function () {
 		.src([
 			//указываем, где брать исходники
 			"node_modules/normalize.css/normalize.css",
-			"node_modules/slick-carousel/slick/slick.css",
+			"node_modules/swiper/swiper-bundle.css",
 		])
 		.pipe(sourcemaps.init())
 		.pipe(concat("libs.min.css")) //склеиваем их в один файл с указанным именем
@@ -109,7 +109,7 @@ gulp.task("script", function () {
 		.src([
 			//тут подключаем разные js в общую библиотеку. Отключите то, что вам не нужно.
 			"node_modules/jquery/dist/jquery.js",
-			"node_modules/slick-carousel/slick/slick.js",
+			"node_modules/swiper/swiper-bundle.js",
 		])
 		.pipe(size())
 		.pipe(sourcemaps.init())
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,15 @@
 $(function(){
 
-  $('.car__items').slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    prevArrow: '<button class="slider-arrows slider-arrows__right"><img src="images/icons/arrow-up.svg" alt=""></button>',
-    nextArrow: '<button class="slider-arrows slider-arrows__left"><img src="images/icons/arrow-down.svg" alt=""></button>',
-    infinite: false,
-    vertical: true
-  });
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 6,
+    spaceBetween: 5,
+    mousewheel: true,
+    direction: 'vertical',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    });
 
 });
 
