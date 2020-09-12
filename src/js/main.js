dcdb7d7d277477712f49c602f70824c50a687f5e
AlexDyatlov <aleks78.gooogle@gmail.com>
2020-09-12 20:31:03 +0300

    added shop-content

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 $(function(){
 
+  $('.car__items').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    prevArrow: '<button class="slider-arrows slider-arrows__right"><img src="images/icons/arrow-up.svg" alt=""></button>',
+    nextArrow: '<button class="slider-arrows slider-arrows__left"><img src="images/icons/arrow-down.svg" alt=""></button>',
+    infinite: false,
+    vertical: true
+  });
+
 });
+
